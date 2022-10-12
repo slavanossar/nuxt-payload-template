420f6198a01e11a3b1e0d3d978bc5823092a1274
Slava Nossar <slava.nossar@gmail.com>
2022-10-12 17:19:08 +1100

    upgrade nuxt-apollo to v5

--- lib/apollo-module/runtime/ws.d.ts
+++
@@ -1,5 +0,0 @@
-import type { Client, ClientOptions } from 'graphql-ws';
-export interface RestartableClient extends Client {
-    restart(): void;
-}
-export default function createRestartableClient(options: ClientOptions): RestartableClient;

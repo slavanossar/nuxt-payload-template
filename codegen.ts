7e7f2749a90e046ba685fa9f2a7bc62ccec661bc
Slava Nossar <slava.nossar@gmail.com>
2023-01-25 13:46:16 +1100

    fix graphql paths

--- codegen.ts
+++ codegen.ts
@@ -2,12 +2,12 @@ import { CodegenConfig } from '@graphql-codegen/cli'
 
 const config: CodegenConfig = {
   schema: 'http://localhost:3001/api/graphql',
-  documents: 'graphql/payload/**/*.gql',
+  documents: 'graphql/**/*.gql',
   generates: {
     './payload-types.d.ts': {
       plugins: ['typescript', 'typescript-operations'],
     },
-    './graphql/payload/exports.d.ts': {
+    './graphql/exports.d.ts': {
       plugins: ['typescript-vue-apollo'],
       config: {
         withCompositionFunctions: false,

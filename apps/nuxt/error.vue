724d8a0a6fb59da6fe6f0c7fb6008b013bde4ac5
Slava Nossar <slava.nossar@gmail.com>
2023-06-01 13:07:41 +1000

    display error

--- apps/nuxt/error.vue
+++ apps/nuxt/error.vue
@@ -1,12 +1,15 @@
 <template>
   <div>
+    <p v-text="props.error.message" />
     <button @click="handleError">Clear errors</button>
   </div>
 </template>
 
 <script lang="ts" setup>
+import type { H3Error } from 'h3'
+
 interface Props {
-  error: object
+  error: H3Error
 }
 const props = defineProps<Props>()
 

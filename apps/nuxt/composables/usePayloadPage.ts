93ac38ca7fc61df4f08230d1440632eb4077afdf
Slava Nossar <slava.nossar@gmail.com>
2024-02-05 18:54:25 +1100

    update usePayloadPage composable to use nuxtApp.runWithContext() with useSeoMeta()

--- apps/nuxt/composables/usePayloadPage.ts
+++ apps/nuxt/composables/usePayloadPage.ts
@@ -4,42 +4,39 @@ import type { Image, Page } from '#payload/types'
 import { useGlobalsStore } from '@/stores/globals'
 
 export default async function (query: DocumentNode) {
+  const nuxtApp = useNuxtApp()
   const config = useRuntimeConfig()
   const globalsStore = useGlobalsStore()
 
   const doc = ref<Page | null>(null)
 
-  watch(doc, (newDocValue) => {
-    if (newDocValue) {
-      const docMeta = newDocValue.meta
-      const seoMeta: UseSeoMetaInput = { title: newDocValue.template }
-
-      seoMeta.title = docMeta?.title || seoMeta.title
-
-      seoMeta.ogTitle = seoMeta.twitterTitle = `${seoMeta.title} | ${
-        globalsStore.site?.meta?.title || config.public.siteName
-      }`
-
-      if (docMeta?.description) {
-        seoMeta.description =
-          seoMeta.ogDescription =
-          seoMeta.twitterDescription =
-            docMeta.description
-      }
-
-      if (docMeta?.image) {
-        seoMeta.ogImage =
-          checkRelation<Image>(docMeta?.image)?.sizes?.opengraph?.url || ''
-      }
-
-      useSeoMeta(seoMeta)
-    }
-  })
-
   const { data } = await useAsyncQuery<PayloadQuery>(query)
 
   if (data.value?.Pages?.docs.length) {
     doc.value = data.value.Pages?.docs[0]
+
+    const docMeta = doc.value.meta
+    const seoMeta: UseSeoMetaInput = { title: doc.value.template }
+
+    seoMeta.title = docMeta?.title || seoMeta.title
+
+    seoMeta.ogTitle = seoMeta.twitterTitle = `${seoMeta.title} | ${
+      globalsStore.site?.meta?.title || config.public.siteName
+    }`
+
+    if (docMeta?.description) {
+      seoMeta.description =
+        seoMeta.ogDescription =
+        seoMeta.twitterDescription =
+          docMeta.description
+    }
+
+    if (docMeta?.image) {
+      seoMeta.ogImage =
+        checkRelation<Image>(docMeta?.image)?.sizes?.opengraph?.url || ''
+    }
+
+    nuxtApp.runWithContext(() => useSeoMeta(seoMeta))
   } else {
     throw createError({
       statusCode: 404,

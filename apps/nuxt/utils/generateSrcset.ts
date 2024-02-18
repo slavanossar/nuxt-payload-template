f838412f0947d5f4185cf970eadef14a60d6cdd3
Slava Nossar <slava.nossar@gmail.com>
2024-02-18 15:41:30 +1100

    fix generateSrcset utility

--- apps/nuxt/utils/generateSrcset.ts
+++ apps/nuxt/utils/generateSrcset.ts
@@ -1,8 +1,8 @@
 import type { Image } from '#payload/types'
 
-export default function ({ webp }: Image): string {
-  return webp?.sizes
-    ? Object.entries(webp.sizes)
+export default function ({ sizes }: Image): string {
+  return sizes
+    ? Object.entries(sizes)
         .filter(([name, { url }]) => url && name !== 'opengraph')
         .map(([_, { url, width }]) => `${encodeURI(url!)} ${width}w`)
         .join()

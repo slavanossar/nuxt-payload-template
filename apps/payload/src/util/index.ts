27beb7cbed5156f3f39a9b9d7dc4d779a00cf43e
Slava Nossar <slava.nossar@gmail.com>
2024-02-19 19:06:39 +1100

    add livePreviewBreakpoints util

---
+++ apps/payload/src/util/index.ts
@@ -0,0 +1,32 @@
+export const livePreviewBreakpoints = [
+  {
+    name: 'mobile',
+    label: 'Mobile',
+    width: 375,
+    height: 667,
+  },
+  {
+    name: 'tabletPortrait',
+    label: 'Tablet (Portrait)',
+    width: 768,
+    height: 1024,
+  },
+  {
+    name: 'tabletLandscape',
+    label: 'Tablet (Landscape)',
+    width: 1024,
+    height: 768,
+  },
+  {
+    name: 'desktop',
+    label: 'Desktop',
+    width: 1280,
+    height: 800,
+  },
+  {
+    name: 'desktopWide',
+    label: 'Desktop (Wide)',
+    width: 1536,
+    height: 960,
+  },
+]

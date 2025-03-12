3fe68e62b969fd42741b7b66de4eb301223cf664
Slava Nossar <slava.nossar@gmail.com>
2025-03-12 11:28:17 +1100

    render PayloadSvg without wrapper div

--- apps/nuxt/components/payload/PayloadSvg.vue
+++ apps/nuxt/components/payload/PayloadSvg.vue
@@ -1,6 +1,5 @@
 <template>
-  <!-- eslint-disable-next-line vue/no-v-html -->
-  <div v-if="html" v-html="html" />
+  <component v-if="svgComponent" :is="svgComponent" />
 </template>
 
 <script setup lang="ts">
@@ -12,17 +11,33 @@ interface PayloadSvgProps {
 
 const props = defineProps<PayloadSvgProps>()
 const svg = useRelationshipField(props.svg)
-const html = ref('')
+const svgComponent = ref<ReturnType<typeof h> | null>(null)
 
 async function fetchSvg() {
   if (svg.value) {
-    html.value = await $fetch<Blob>(svg.value.url!)
-      .then((res) => {
-        return res.text()
-      })
-      .catch(() => {
-        return ''
-      })
+    const svgString = await $fetch<Blob>(svg.value.url!)
+      .then((res) => res.text())
+      .catch(() => '')
+
+    if (svgString) {
+      const div = document.createElement('div')
+      div.innerHTML = svgString
+      const svgElement = div.querySelector('svg')
+
+      if (svgElement) {
+        const attrs: Record<string, string> = {}
+        Array.from(svgElement.attributes).forEach((attr) => {
+          attrs[attr.name] = attr.value
+        })
+
+        svgComponent.value = h('svg', {
+          ...attrs,
+          innerHTML: svgElement.innerHTML,
+        })
+      }
+    } else {
+      svgComponent.value = null
+    }
   }
 }
 

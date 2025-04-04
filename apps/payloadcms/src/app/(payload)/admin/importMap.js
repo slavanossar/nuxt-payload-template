670176e7ffc547c00174cbb8936925ba9b55d4a5
Slava Nossar <slava.nossar@gmail.com>
2025-04-04 17:47:08 +1100

    regenerate importmap

--- apps/payloadcms/src/app/(payload)/admin/importMap.js
+++ apps/payloadcms/src/app/(payload)/admin/importMap.js
@@ -5,6 +5,7 @@ import { MetaImageComponent as MetaImageComponent_a8a977ebc872c5d5ea7ee689724c08
 import { PreviewComponent as PreviewComponent_a8a977ebc872c5d5ea7ee689724c0860 } from '@payloadcms/plugin-seo/client'
 import { default as default_296471cde0ccd15e25e98a670243e40f } from '@payload/graphics/Icon.tsx'
 import { default as default_518116d937b6c07cd12fdd8e69ee3df0 } from '@payload/graphics/Logo.tsx'
+import { S3ClientUploadHandler as S3ClientUploadHandler_f97aa6c64367fa259c5bc0567239ef24 } from '@payloadcms/storage-s3/client'
 
 export const importMap = {
   "@payloadcms/plugin-seo/client#OverviewComponent": OverviewComponent_a8a977ebc872c5d5ea7ee689724c0860,
@@ -13,5 +14,6 @@ export const importMap = {
   "@payloadcms/plugin-seo/client#MetaImageComponent": MetaImageComponent_a8a977ebc872c5d5ea7ee689724c0860,
   "@payloadcms/plugin-seo/client#PreviewComponent": PreviewComponent_a8a977ebc872c5d5ea7ee689724c0860,
   "@payload/graphics/Icon.tsx#default": default_296471cde0ccd15e25e98a670243e40f,
-  "@payload/graphics/Logo.tsx#default": default_518116d937b6c07cd12fdd8e69ee3df0
+  "@payload/graphics/Logo.tsx#default": default_518116d937b6c07cd12fdd8e69ee3df0,
+  "@payloadcms/storage-s3/client#S3ClientUploadHandler": S3ClientUploadHandler_f97aa6c64367fa259c5bc0567239ef24
 }

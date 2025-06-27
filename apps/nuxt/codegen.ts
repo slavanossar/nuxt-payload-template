49362ac7a4d878300683c87989b1641891fe9862
Slava Nossar <slava.nossar@gmail.com>
2025-06-27 15:24:58 +1000

    dont use hostname for codegen

--- apps/nuxt/codegen.ts
+++ apps/nuxt/codegen.ts
@@ -1,7 +1,7 @@
 import type { CodegenConfig } from '@graphql-codegen/cli'
 
 const config: CodegenConfig = {
-  schema: `${process.env.NEXT_PUBLIC_SITE_URL}${process.env.NEXT_PUBLIC_PAYLOAD_API_ROUTE}/graphql`,
+  schema: `http://localhost:${process.env.PORT}${process.env.NEXT_PUBLIC_PAYLOAD_API_ROUTE}/graphql`,
   documents: 'graphql/**/*.gql',
   generates: {
     './graphql/exports.d.ts': {

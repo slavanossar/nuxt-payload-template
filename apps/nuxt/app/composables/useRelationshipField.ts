8382e47b18260f43e81911218b45e31906b278d5
Slava Nossar <slavanossar@users.noreply.github.com>
2025-12-04 13:34:13 +1100

    Refactor useRelationshipField to exclude strings

--- apps/nuxt/app/composables/useRelationshipField.ts
+++ apps/nuxt/app/composables/useRelationshipField.ts
@@ -1,25 +1,31 @@
 type Single<T> = MaybeRef<string | T | null | undefined>
 type Many<T> = MaybeRef<(string | T)[] | null | undefined>
 
-export function useRelationshipField<T>(field: Single<T>): Ref<T | null>
+type ExcludeString<T> = Exclude<T, string>
 
-export function useRelationshipField<T>(field: Many<T>): Ref<T[]>
+export function useRelationshipField<T extends PayloadCollection>(
+  field: Single<T>,
+): Ref<ExcludeString<T> | null>
+
+export function useRelationshipField<T extends PayloadCollection>(
+  field: Many<T>,
+): Ref<ExcludeString<T>[]>
 
 export function useRelationshipField<T extends PayloadCollection>(
   field: Single<T> | Many<T>,
-): Ref<T | null> | Ref<T[]> {
+): Ref<ExcludeString<T> | null> | Ref<ExcludeString<T>[]> {
   const initialValue = unref(field)
   const isArrayField = Array.isArray(initialValue)
 
   if (isArrayField) {
-    const docs = ref<T[]>([]) as Ref<T[]>
+    const docs = ref<ExcludeString<T>[]>([]) as Ref<ExcludeString<T>[]>
 
     watchEffect(() => {
       const fieldUnref = unref(field) as (string | T)[] | null | undefined
 
       if (fieldUnref) {
         docs.value = fieldUnref.filter(
-          (item): item is T => typeof item !== 'string',
+          (item): item is ExcludeString<T> => typeof item !== 'string',
         )
       } else {
         docs.value = []
@@ -28,13 +34,15 @@ export function useRelationshipField<T extends PayloadCollection>(
 
     return docs
   } else {
-    const doc = ref<T | null>(null) as Ref<T | null>
+    const doc = ref<ExcludeString<T> | null>(
+      null,
+    ) as Ref<ExcludeString<T> | null>
 
     watchEffect(() => {
       const fieldUnref = unref(field) as string | T | null | undefined
 
       if (fieldUnref && typeof fieldUnref !== 'string') {
-        doc.value = fieldUnref
+        doc.value = fieldUnref as ExcludeString<T>
       } else {
         doc.value = null
       }
